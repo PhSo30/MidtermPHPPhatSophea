a5e0c04a2a51449c709e7a805cf505fc773e0f31
Sophea <sopheaisanidiot@gmail.com>
2026-01-12 10:25:57 +0700

    get post

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include './includes/header.inc.php';
+include './includes/novbar.inc.php';
+// $_GET['pages'];
+// include_once './pages/'.$_GET['pages'].'.php';
+
+$available_pages = ['login', 'register']; //create array
+
+if (isset($_GET['page'])) {
+    //     echo $_GET['page'];
+
+    // include './pages/' . $_GET['page'] . '.php';
+    $page = $_GET['page'];
+    if (in_array($page, $available_pages)) { //check if value in array
+        include './pages/' . $page . '.php';
+    } else {
+        echo '<h1>Error 404</h1>';
+    }
+
+
+} else {
+    echo '<h1>Home Page</h1>';
+}
+include './includes/footer.inc.php';
+?>
--- pages/register.php
+++ pages/register.php
@@ -1,26 +1,31 @@
 <?php
-include_once '../includes/header.inc.php';
-include_once '../includes/footer.inc.php';
-include_once '../includes/novbar.inc.php';
+// include_once '../includes/header.inc.php';
+// include_once '../includes/footer.inc.php';
+// include_once '../includes/novbar.inc.php';
+
+    if (isset($_POST['username'])) {
+        echo $_POST['username'];
+    }
 ?>
 
 <body>
 
 
-    <form class="col-md-8 col-lg-6 mx-auto">
+    <form method="post" action="./?page=register" class="col-md-8 col-lg-6 mx-auto">
         <h3>Register</h3>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input name="username" type="email" class="form-control" id="exampleInputEmail1"
+                aria-describedby="emailHelp">
             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <input name="confirmpassword" type="password" class="form-control" id="exampleInputPassword1">
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
