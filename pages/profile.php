87cca2b8c9dd9538e342482a5cd17ac2efa458ca
Sophea <sopheaisanidiot@gmail.com>
2026-02-12 12:54:31 +0700

    fix

--- pages/profile.php
+++ pages/profile.php
@@ -6,13 +6,13 @@ $oldPasswdErr = $newPasswdErr = '';
 if (!empty($_SESSION['Profile Message alert'])) {
     echo $_SESSION['Profile Message alert'];
     $_SESSION['Profile Message alert'] = '';
-}else{
+} else {
     echo '<div class="alert alert-info" role="alert">No message to display.</div>';
 }
 
 
 
-$photo = empty(loggedInUser() -> photo) ? 'emptyuser.png' : loggedInUser()->photo;
+$photo = empty(loggedInUser()->photo) ? 'emptyuser.png' : loggedInUser()->photo;
 
 if (isset($_POST['changePasswd'], $_POST['oldPasswd'], $_POST['newPasswd'], $_POST['confirmNewPasswd'])) {
     $oldPasswd = trim($_POST['oldPasswd']);
@@ -42,8 +42,8 @@ if (isset($_POST['changePasswd'], $_POST['oldPasswd'], $_POST['newPasswd'], $_PO
     }
 }
 if (isset($_POST['deletePhoto'])) {
-    $message = deleteUserImage($_SESSION['user_id']);
-    if($message === true){
+    $message = deleteUserImage();
+    if ($message === true) {
         $_SESSION['Profile Message alert'] = '<div class="alert alert-success" role="alert">Image deleted successfully!</div>';
         header('Location: ./?page=profile');
         exit();
@@ -106,21 +106,20 @@ if (isset($_POST['uploadPhoto'])) {
                 </label>
             </div>
             <div class="d-flex justify-content-center">
-                <button type="submit" name="deletePhoto" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                <button type="submit" name="uploadPhoto" class="btn btn-success" 
-                <?php if(!empty(loggedInUser()->photo))
-                    echo 'onclick="return confirm(\'Are you sure? If you upload a new image, the old one will be replaced.\')"' 
-                ?>
-                >Upload</button>
-            </div>
-        </form>
-    </div>
-    <div class="col-6">
-        <form method="post" action="./?page=profile" class="col-md-10 col-lg-6 mx-auto">
-            <h3>Change Password</h3>
-            <div class="mb-3">
-                <label class="form-label">Old Password</label>
-                <input value="<?php echo $oldPasswd ?>" name="oldPasswd" type="password" class="form-control 
+                <button type="submit" name="deletePhoto" class="btn btn-danger"
+                    onclick="return confirm('Are you sure?')">Delete</button>
+                <button type="submit" name="uploadPhoto" class="btn btn-success" <?php if (!empty(loggedInUser()->photo))
+                    echo 'onclick="return confirm(\'Are you sure? If you upload a new image, the old one will be replaced.\')"'
+                        ?>>Upload</button>
+                </div>
+            </form>
+        </div>
+        <div class="col-6">
+            <form method="post" action="./?page=profile" class="col-md-10 col-lg-6 mx-auto">
+                <h3>Change Password</h3>
+                <div class="mb-3">
+                    <label class="form-label">Old Password</label>
+                    <input value="<?php echo $oldPasswd ?>" name="oldPasswd" type="password" class="form-control 
                 <?php echo empty($oldPasswdErr) ? '' : 'is-invalid' ?>">
                 <div class="invalid-feedback">
                     <?php echo $oldPasswdErr ?>
@@ -144,6 +143,7 @@ if (isset($_POST['uploadPhoto'])) {
 </div>
 
 <script>
+
     const profileUpload = document.getElementById('profileUpload');
     const profileImg = document.querySelector('label[for="profileUpload"] img');
     profileUpload.addEventListener('change', function () {
